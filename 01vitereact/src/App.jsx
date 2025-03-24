bd2af4b82505d320a3a42e6476abd2e353469949
Raja Mahanty <rajamahanty2@gmail.com>
2025-03-24 10:21:50 +0530

    initial commit

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,13 @@
+import Chai from "./chai"
+
+function App() {
+  return (
+    <>
+      <p>Before that make it first</p>
+      <Chai />
+      <h3>DONE EATING TEA, NOW SLEEP</h3>
+    </>
+  )
+}
+
+export default App
---
+++ 01vitereact/src/chai.jsx
@@ -0,0 +1,7 @@
+function Chai() {
+    return (
+        <h1>Let's eat some Chai</h1>
+    );
+}
+
+export default Chai;
